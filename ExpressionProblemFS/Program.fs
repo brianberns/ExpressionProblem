43c46c20847b8eed5bda592980eb108f34be5979
Brian Berns <brianberns@gmail.com>
2020-06-06 02:09:25 -0400

    Added F# algebra.

--- ExpressionProblemFS/Program.fs
+++ ExpressionProblemFS/Program.fs
@@ -1,18 +1,53 @@
-﻿type Expr =
-    | Literal of int
-    | Add of Expr * Expr
+﻿module Simple =
 
-let rec eval = function
-    | Literal n -> n
-    | Add (a, b) -> eval a + eval b
+    type Expr =
+        | Literal of int
+        | Add of Expr * Expr
 
-let rec print = function
-    | Literal n -> n.ToString()
-    | Add (a, b) -> sprintf "(%s + %s)" (print a) (print b)
+    let rec eval = function
+        | Literal n -> n
+        | Add (a, b) -> eval a + eval b
+
+    let rec print = function
+        | Literal n -> n.ToString()
+        | Add (a, b) -> sprintf "(%s + %s)" (print a) (print b)
+
+    let test () =
+        let expr = Add (Literal 1, (Add (Literal 2, Literal 3)))
+        printfn ""
+        printfn "Simple test"
+        printfn "   Eval: %A" <| eval expr
+        printfn "   Print: %s" <| print expr
+
+module Algebra =
+
+    type ExprAlgebra<'T> =
+        abstract member Literal : int -> 'T
+        abstract member Add : 'T -> 'T -> 'T
+
+    type EvalAlgebra () =
+        interface ExprAlgebra<unit -> int> with
+            member __.Literal n =
+                fun () -> n
+            member __.Add a b =
+                fun () -> a () + b ()
+
+    let test () =
+
+        let createTestExpr (factory : ExprAlgebra<_>) =
+            factory.Add
+                (factory.Literal 1)
+                (factory.Add
+                    (factory.Literal 2)
+                    (factory.Literal 3))
+
+        let expr = createTestExpr <| EvalAlgebra ()
+        printfn ""
+        printfn "Algebra test"
+        printfn "   Eval: %A" <| expr ()
 
 [<EntryPoint>]
 let main argv =
-    let expr = Add (Literal 1, (Add (Literal 2, Literal 3)))
-    printfn "Eval: %A" <| eval expr
-    printfn "Print: %s" <| print expr
+    Simple.test ()
+    Algebra.test ()
     0
